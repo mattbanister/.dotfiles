01c5dcce18b9684b17543ea0a9dc46980ae6a2dc
Matthew Banister <matthew.banister@gmail.com>
2025-08-17 02:13:20 -0400

    Add some neovim config

---
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -0,0 +1,15 @@
+return { 
+  "ellisonleao/gruvbox.nvim",
+  lazy = false, 
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      italic = {
+        comments = false
+      },
+      contrast = "hard"
+    })
+    vim.o.background = "dark"
+    vim.cmd("colorscheme gruvbox")
+  end
+}
