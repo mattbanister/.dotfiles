f14a20c3975918652e007d952f12d11e5ddd1adf
Matthew Banister <matthew.banister@gmail.com>
2025-08-17 20:52:22 -0400

    Switch theme to gruvbox-material. Other style tweaks

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,15 +1,16 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  'sainnhe/gruvbox-material',
   lazy = false,
-  priority = 1000,
   config = function()
-    require("gruvbox").setup({
-      italic = {
-        comments = false
-      },
-      contrast = "soft"
-    })
-    vim.o.background = "dark"
-    vim.cmd("colorscheme gruvbox")
-  end
-}
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_foreground = "material"
+    vim.g.gruvbox_material_background = "soft"
+    vim.g.gruvbox_material_ui_contrast = "low"
+    vim.g.gruvbox_material_float_style = "dim"
+    vim.g.gruvbox_material_enable_italic = 0
+    vim.g.gruvbox_material_disable_italic_comment = 1
+    vim.g.gruvbox_material_cursor = "red"
+    vim.g.gruvbox_material_disable_terminal_colors = 1
+    vim.cmd.colorscheme('gruvbox-material')
+  end,
+ }
