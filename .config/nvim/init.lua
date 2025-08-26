22e3e7dceb04ed98ef95d0b33375c8c0a781045b
Matthew Banister <matthew.banister@gmail.com>
2025-08-25 20:45:46 -0400

    Switch from none-ls to confom

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,7 @@ require('lazy').setup {
   require 'plugins.telescope',
   require 'plugins.lsp',
   require 'plugins.autocompletion',
-  require 'plugins.none-ls',
+  require 'plugins.conform',
   require 'plugins.gitsigns',
   require 'plugins.alpha',
   require 'plugins.indent-blankline',
