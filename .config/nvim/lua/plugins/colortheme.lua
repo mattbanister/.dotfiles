4be7d4016c132eba99b8eb094d852d669a40045e
Matthew Banister <matthew.banister@gmail.com>
2026-05-25 15:13:03 -0400

    Switch to kanagawa wave

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,27 +1,9 @@
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000, -- Load this before all other plugins
-    config = function()
-      require('catppuccin').setup {
-        flavour = 'mocha', -- latte, frappe, macchiato, mocha
-        transparent_background = false,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = '',
-          },
-        },
-      }
-
-      -- Set the colorscheme
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
+  'rebelot/kanagawa.nvim',
+  lazy = false, -- Load during startup
+  priority = 1000, -- Load before other plugins
+  config = function()
+    -- Load the colorscheme here
+    vim.cmd 'colorscheme kanagawa-wave'
+  end,
 }
